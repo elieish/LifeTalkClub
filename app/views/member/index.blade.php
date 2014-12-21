a9af8e53b434f11635a98aa607ce1f48dcaf0182
Elie Ishimwe <elieish@gmail.com>
2014-12-21 11:51:56 +0200

    Package updates

--- app/views/member/index.blade.php
+++ app/views/member/index.blade.php
@@ -95,41 +95,41 @@
                                 <tr>
                                     <td>2</td>
                                     <td>25</td>
-                                    <td>R2500</td>
-                                    <td>R1500 Grocery Voucher + 350 INV</td>
+                                    <td>R5000</td>
+                                    <td>R2500 Grocery Voucher Grocery + HNH Meat</td>
                                 </tr>
                                 <tr>
                                     <td>3</td>
                                     <td>125</td>
-                                    <td>R6 000</td>
-                                    <td>R1500 Container Business</td>
+                                    <td>R18 250</td>
+                                    <td>R2500 Grocery Voucher Grocery + HNH Meat</td>
                                 </tr>
                                 <tr>
                                     <td>4</td>
                                     <td>625</td>
-                                    <td>R32 000</td>
-                                    <td>R5000 Grocery Voucher + Afro Jazz</td>
+                                    <td>R91 250</td>
+                                    <td>R5000 Buy Presless Linen and AMC Pots</td>
                                 </tr>
                                 <tr>
                                     <td>5</td>
                                     <td>3125</td>
-                                    <td>R187 500</td>
-                                    <td>R25 000 AMC Pots + Presless Metropolitan Life</td>
+                                    <td>R468 750</td>
+                                    <td>R7500 Voucher Grocery</td>
                                 </tr>
                                 <tr>
                                     <td>6</td>
                                     <td>15625</td>
-                                    <td>R930 000</td>
-                                    <td>R25 000 Bonus + Medizone Machine </td>
+                                    <td>R2 143 750</td>
+                                    <td>Car for value of R200K or Luxury Lifestyle Furniture of your choice </td>
                                 </tr>
                                 <tr>
                                     <td>7</td>
                                     <td>78125</td>
-                                    <td>R1 500 687.500</td>
-                                    <td>R25 000 Bonus ,R100 000 Forex & 247 My Deposit for 12 months</td>
+                                    <td>R4 718 750</td>
+                                    <td>House for 2 Millions</td>
                                 </tr>
 
-                           </table>
+                            </table>
 
                             <li class="plan-action">
                                 <a href="members/signup" class="btn btn-primary">Sign up</a>
@@ -137,42 +137,135 @@
                         </ul>
                     </div>
 
-                    <!-- <div class="col-sm-4 plan price-three wow fadeInDown">
-                        <img src="images/ribon_one.png">
+                    <div class="col-sm-4 plan price-three wow fadeInDown">
+
                         <ul>
                             <li class="heading-three">
                                 <h1>Gold</h1>
-                                <span>R1500/Month</span>
+                                <span>R1500/Month Maintenable</span>
                             </li>
-                            <li>50 Gb Disk Space</li>
-                            <li>8GB Dadicated Ram</li>
-                            <li>Unlimited Addon Domain</li>
-                            <li>Unlimited Email Account</li>
-                            <li>24/7 Support</li>
+                            <table border='1'>
+                                <tr>
+                                    <th>Level</th>
+                                    <th>Referrals</th>
+                                    <th>Once-Off Earnings</th>
+                                    <th>Extra Benefits</th>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>5</td>
+                                    <td>R3000</td>
+                                    <td>R1500 Voucher</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>25</td>
+                                    <td>R15000</td>
+                                    <td>R2000 Voucher HNH Meat </td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>125</td>
+                                    <td>R37 500</td>
+                                    <td>R3500 Clothing Voucher</td>
+                                </tr>
+                                <tr>
+                                    <td>4</td>
+                                    <td>625</td>
+                                    <td>R183 500</td>
+                                    <td>R4000 Voucher</td>
+                                </tr>
+                                <tr>
+                                    <td>5</td>
+                                    <td>3125</td>
+                                    <td>R927 500</td>
+                                    <td>R7500 Medizone Machine/td>
+                                </tr>
+                                <tr>
+                                    <td>6</td>
+                                    <td>15625</td>
+                                    <td>R4 687 500</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>7</td>
+                                    <td>78125</td>
+                                    <td>R20 437 500</td>
+                                    <td>MRC,R7500 Petrol,R7500 Entertainment,5 yrs Free Service Play,15 Days Holiday of your choice meals & flight paid & security guard for your house while you are away</td>
+                                </tr>
+
+                            </table>
+
                             <li class="plan-action">
                                 <a href="members/signup" class="btn btn-primary">Sign up</a>
                             </li>
                         </ul>
-                    </div> -->
+                    </div>
 
 
-               <!--      <div class="col-sm-4 plan price-three wow fadeInDown">
-                        <img src="images/ribon_one.png">
+               <div class="col-sm-4 plan price-three wow fadeInDown">
                         <ul>
-                            <li class="heading-three">
-                                <h1>Diamond</h1>
-                                <span>R5000/Month</span>
+                            <li class="heading-four">
+                                <h1>Platinum</h1>
+                                <span>R3000/Month Maintenable</span>
                             </li>
-                            <li>50 Gb Disk Space</li>
-                            <li>8GB Dadicated Ram</li>
-                            <li>Unlimited Addon Domain</li>
-                            <li>Unlimited Email Account</li>
-                            <li>24/7 Support</li>
+                            <table border='1'>
+                                <tr>
+                                    <th>Level</th>
+                                    <th>Referrals</th>
+                                    <th>Once-Off Earnings</th>
+                                    <th>Extra Benefits</th>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>5</td>
+                                    <td>R6000</td>
+                                    <td>R3000 Voucher</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>25</td>
+                                    <td>R30 000</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>125</td>
+                                    <td>R75 000</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>4</td>
+                                    <td>625</td>
+                                    <td>R375 000</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>5</td>
+                                    <td>3125</td>
+                                    <td>R1 875 000</td>
+                                    <td>Investment for 100 Ks</td>
+                                </tr>
+                                <tr>
+                                    <td>6</td>
+                                    <td>15525</td>
+                                    <td>R9 075 000</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>7</td>
+                                    <td>78125</td>
+                                    <td>R40 075 000</td>
+                                    <td>House up to 6 Mils + Range</td>
+                                </tr>
+
+                            </table>
+
                             <li class="plan-action">
                                 <a href="" class="btn btn-primary">Sign up</a>
                             </li>
                         </ul>
-                    </div> -->
+                </div>
 
                   <!--   <div class="col-sm-4 plan price-three wow fadeInDown">
                         <img src="images/ribon_one.png">

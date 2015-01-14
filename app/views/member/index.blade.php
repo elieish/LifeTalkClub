0f16be350a53ba0cc60c4b6329b19d4f2b2b918d
Elie Ishimwe <elieish@gmail.com>
2015-01-14 08:34:38 +0200

    Fixed members page spacing issues

--- app/views/member/index.blade.php
+++ app/views/member/index.blade.php
@@ -26,43 +26,43 @@
                                     <td>1</td>
                                     <td>5</td>
                                     <td>R800</td>
-                                    <td>R300 Master Cards Account only</td>
+                                    <td>R150 Voucher</td>
                                 </tr>
                                 <tr>
                                     <td>2</td>
                                     <td>25</td>
                                     <td>R2500</td>
-                                    <td>R850 Grocery Voucher + 350 INV</td>
+                                    <td>R850 Voucher/td>
                                 </tr>
                                 <tr>
                                     <td>3</td>
                                     <td>125</td>
                                     <td>R6 000</td>
-                                    <td>R1500 Container Business</td>
+                                    <td>Business Startup</td>
                                 </tr>
                                 <tr>
                                     <td>4</td>
                                     <td>625</td>
                                     <td>R32 000</td>
-                                    <td>R5000 Grocery Voucher + Afro Jazz</td>
+                                    <td>R5000 Voucher</td>
                                 </tr>
                                 <tr>
                                     <td>5</td>
                                     <td>3125</td>
                                     <td>R187 500</td>
-                                    <td>R25 000 AMC Pots + Presless Metropolitan Life</td>
+                                    <td>R25 000 Recurring Bonus</td>
                                 </tr>
                                 <tr>
                                     <td>6</td>
                                     <td>15625</td>
                                     <td>R930 000</td>
-                                    <td>R25 000 Bonus + Medizone Machine </td>
+                                    <td>R25 000 Recurring Bonus</td>
                                 </tr>
                                 <tr>
                                     <td>7</td>
                                     <td>78125</td>
                                     <td>R1 500 687.500</td>
-                                    <td>R25 000 Bonus ,R100 000 Forex & 247 My Deposit for 12 months</td>
+                                    <td>R25 000 Bonus</td>
                                 </tr>
 
                            </table>
@@ -90,43 +90,43 @@
                                     <td>1</td>
                                     <td>5</td>
                                     <td>R2500</td>
-                                    <td>R1500 Grocery Voucher</td>
+                                    <td>R1500 Voucher</td>
                                 </tr>
                                 <tr>
                                     <td>2</td>
                                     <td>25</td>
                                     <td>R5000</td>
-                                    <td>R2500 Grocery Voucher Grocery + HNH Meat</td>
+                                    <td>R2500 Voucher</td>
                                 </tr>
                                 <tr>
                                     <td>3</td>
                                     <td>125</td>
                                     <td>R18 250</td>
-                                    <td>R2500 Grocery Voucher Grocery + HNH Meat</td>
+                                    <td>R2500 Voucher</td>
                                 </tr>
                                 <tr>
                                     <td>4</td>
                                     <td>625</td>
                                     <td>R91 250</td>
-                                    <td>R5000 Buy Presless Linen and AMC Pots</td>
+                                    <td>Presless Linen and AMC Pots</td>
                                 </tr>
                                 <tr>
                                     <td>5</td>
                                     <td>3125</td>
                                     <td>R468 750</td>
-                                    <td>R7500 Voucher Grocery</td>
+                                    <td>R7500 Voucher </td>
                                 </tr>
                                 <tr>
                                     <td>6</td>
                                     <td>15625</td>
                                     <td>R2 143 750</td>
-                                    <td>Car for value of R200K or Luxury Lifestyle Furniture of your choice </td>
+                                    <td>Furniture or Car</td>
                                 </tr>
                                 <tr>
                                     <td>7</td>
                                     <td>78125</td>
                                     <td>R4 718 750</td>
-                                    <td>House for 2 Millions</td>
+                                    <td>House</td>
                                 </tr>
 
                             </table>
@@ -161,13 +161,13 @@
                                     <td>2</td>
                                     <td>25</td>
                                     <td>R15000</td>
-                                    <td>R2000 Voucher HNH Meat </td>
+                                    <td>R1500 Voucher</td>
                                 </tr>
                                 <tr>
                                     <td>3</td>
                                     <td>125</td>
                                     <td>R37 500</td>
-                                    <td>R3500 Clothing Voucher</td>
+                                    <td>R1500 Voucher</td>
                                 </tr>
                                 <tr>
                                     <td>4</td>
@@ -179,19 +179,19 @@
                                     <td>5</td>
                                     <td>3125</td>
                                     <td>R927 500</td>
-                                    <td>R7500 Medizone Machine/td>
+                                    <td>R4000 Voucher</td>
                                 </tr>
                                 <tr>
                                     <td>6</td>
                                     <td>15625</td>
                                     <td>R4 687 500</td>
-                                    <td></td>
+                                    <td>R4000 Voucher</td>
                                 </tr>
                                 <tr>
                                     <td>7</td>
                                     <td>78125</td>
                                     <td>R20 437 500</td>
-                                    <td>MRC,R7500 Petrol,R7500 Entertainment,5 yrs Free Service Play,15 Days Holiday of your choice meals & flight paid & security guard for your house while you are away</td>
+                                    <td>.Car + R7500 Petrol pm + R7500 Entertainment + 14 Days Holiday full paid and meals</td>
                                 </tr>
 
                             </table>
@@ -226,37 +226,37 @@
                                     <td>2</td>
                                     <td>25</td>
                                     <td>R30 000</td>
-                                    <td></td>
+                                    <td>R3000 Voucher</td>
                                 </tr>
                                 <tr>
                                     <td>3</td>
                                     <td>125</td>
                                     <td>R75 000</td>
-                                    <td></td>
+                                    <td>R3000 Voucher</td>
                                 </tr>
                                 <tr>
                                     <td>4</td>
                                     <td>625</td>
                                     <td>R375 000</td>
-                                    <td></td>
+                                    <td>R3000 Voucher</td>
                                 </tr>
                                 <tr>
                                     <td>5</td>
                                     <td>3125</td>
                                     <td>R1 875 000</td>
-                                    <td>Investment for 100 Ks</td>
+                                    <td>Investment Options</td>
                                 </tr>
                                 <tr>
                                     <td>6</td>
                                     <td>15525</td>
                                     <td>R9 075 000</td>
-                                    <td></td>
+                                    <td>Investment Options</td>
                                 </tr>
                                 <tr>
                                     <td>7</td>
                                     <td>78125</td>
                                     <td>R40 075 000</td>
-                                    <td>House up to 6 Mils + Range</td>
+                                    <td>House</td>
                                 </tr>
 
                             </table>
